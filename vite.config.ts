e486e6c5d27e6b96cbead51c2c4e8adb3a6ef4b0
Websprint <websprintt@gmail.com>
2026-05-05 17:22:48 +0200

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,9 @@
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig(({mode}) => {
-  const env = loadEnv(mode, '.', '');
-  return {
-    base: '/multiservicios-integrales-alonzo/',
-    plugins: [react(), tailwindcss()],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      },
-    },
-    server: {
-      port: 3000,
-      hmr: process.env.DISABLE_HMR !== 'true',
-    },
-  };
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/multiservicios-integrales-alonzo/',
+  plugins: [react(), tailwindcss()],
 });
